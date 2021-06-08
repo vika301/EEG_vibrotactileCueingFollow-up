ef3dc7f03a2e24a0c3e3b3df04081885a353936f
vic <vbenhauser@uos.de>
2021-06-08 17:19:06 +0200

    FollowUp changes unfinished (paths and other stuff) No data-acess @home

---
+++ Analysis/CleanDataAnalysisFollowUp.m
@@ -0,0 +1,80 @@
+%% Chunk 1
+
+% enter from where you want to load the data and where you want to save it
+addpath(genpath('/work/vbenhauser/eeglab2021.0'))
+dataload_path = '/net/store/nbp/projects/EEG_Tactile/EEG_Tactile_FollowUp/raw_data';
+saving_path = '/net/store/nbp/projects/EEG_Tactile/EEG_Tactile_FollowUp';
+
+% find the preprocessed files
+cd(dataload_path);
+all_files = dir('**/*.set'); % (only loads the files with a .set ending)
+all_files = {all_files.name};
+
+
+% specify if you would like to have the baseline removed
+remove_baseline = true;
+
+% Loop through every file 
+for file = all_files
+    
+% load the cleaned data set that we want to split into the conditions
+eeg_cleaned = pop_loadset(file);
+
+% set a window span for the epochs
+window=[-0.2 .8];
+% epoch continuous data according to the 8 conditions (waist, wrist, ankle,
+% visual) each (oddball and standard stimulus)
+[eeg_cleaned_visual_odd, indices_visual_odd] = pop_epoch( eeg_cleaned, {'1'}, window, 'epochinfo', 'yes');
+[eeg_cleaned_visual_stand, indices_visual_stand] = pop_epoch( eeg_cleaned, {'3'}, window, 'epochinfo', 'yes');
+
+[eeg_cleaned_ankle_swapped_odd, indices_ankle_swapped_odd] = pop_epoch( eeg_cleaned, {'5'}, window, 'epochinfo', 'yes');
+[eeg_cleaned_ankle_swapped_stand, indices_ankle_swapped_stand] = pop_epoch( eeg_cleaned, {'7'}, window, 'epochinfo', 'yes');
+
+[eeg_cleaned_ankle_odd, indices_ankle_odd] = pop_epoch( eeg_cleaned, {'9'}, window, 'epochinfo', 'yes');
+[eeg_cleaned_ankle_stand, indices_ankle_stand] = pop_epoch( eeg_cleaned, {'11'}, window, 'epochinfo', 'yes');
+
+[eeg_cleaned_visual_swapped_odd, indices_visual_swapped_odd] = pop_epoch( eeg_cleaned, {'14'}, window, 'epochinfo', 'yes');
+[eeg_cleaned_visual_swapped_stand, indices_visual_swapped_stand] = pop_epoch( eeg_cleaned, {'13'}, window, 'epochinfo', 'yes');
+
+eeg_cleaned_visual_odd.orig_indices = indices_visual_odd;
+eeg_cleaned_visual_stand.orig_indices = indices_visual_stand;
+eeg_cleaned_ankle_swapped_odd.orig_indices = indices_ankle_swapped_odd;
+eeg_cleaned_ankle_swapped_stand.orig_indices = indices_ankle_swapped_stand;
+eeg_cleaned_ankle_odd.orig_indices = indices_ankle_odd;
+eeg_cleaned_ankle_stand.orig_indices = indices_ankle_stand;
+eeg_cleaned_visual_swapped_odd.orig_indices = indices_visual_swapped_odd;
+eeg_cleaned_visual_swapped_stand.orig_indices = indices_visual_swapped_stand;
+
+% remove the baseline if desired 
+if remove_baseline
+fprintf('Remove baseline: %s', string(remove_baseline));
+eeg_cleaned_visual_odd = pop_rmbase(eeg_cleaned_visual_odd, [-199 0]);
+eeg_cleaned_visual_stand = pop_rmbase(eeg_cleaned_visual_stand, [-199 0]);
+eeg_cleaned_ankle_swapped_odd = pop_rmbase(eeg_cleaned_ankle_swapped_odd, [-199 0]);
+eeg_cleaned_ankle_swapped_stand = pop_rmbase(eeg_cleaned_ankle_swapped_stand, [-199 0]);
+eeg_cleaned_ankle_odd = pop_rmbase(eeg_cleaned_ankle_odd, [-199 0]);
+eeg_cleaned_ankle_stand = pop_rmbase(eeg_cleaned_ankle_stand, [-199 0]);
+eeg_cleaned_visual_swapped_odd = pop_rmbase(eeg_cleaned_visual_swapped_odd, [-199 0]);
+eeg_cleaned_visual_swapped_stand = pop_rmbase(eeg_cleaned_visual_swapped_stand, [-199 0]);
+end
+
+
+
+% save the file 
+cd(saving_path);
+eeg_cleaned.filename = eeg_cleaned.filename(1:end-4);
+% the waist condition
+pop_saveset(eeg_cleaned_visual_odd, 'filename', sprintf('%s_cleaned_visual_odd.set', eeg_cleaned.filename), 'filepath', eeg_cleaned_visual_odd.filepath);
+pop_saveset(eeg_cleaned_visual_stand, 'filename', sprintf('%s_cleaned_visual_stand.set', eeg_cleaned.filename), 'filepath', eeg_cleaned_visual_stand.filepath);
+% the wrist condition
+pop_saveset(eeg_cleaned_ankle_swapped_odd, 'filename', sprintf('%s_cleaned_ankle_swapped_odd.set', eeg_cleaned.filename), 'filepath', eeg_cleaned_ankle_swapped_odd.filepath);
+pop_saveset(eeg_cleaned_ankle_swapped_stand, 'filename', sprintf('%s_cleaned_ankle_swapped_stand.set', eeg_cleaned.filename), 'filepath', eeg_cleaned_ankle_swapped_stand.filepath);
+% the ankle condition
+pop_saveset(eeg_cleaned_ankle_odd, 'filename', sprintf('%s_cleaned_ankle_odd.set', eeg_cleaned.filename), 'filepath', eeg_cleaned_ankle_odd.filepath);
+pop_saveset(eeg_cleaned_ankle_stand, 'filename', sprintf('%s_cleaned_ankle_stand.set', eeg_cleaned.filename), 'filepath', eeg_cleaned_ankle_stand.filepath);
+% the visual condition
+pop_saveset(eeg_cleaned_visual_swapped_odd, 'filename', sprintf('%s_cleaned_visual_swapped_odd.set', eeg_cleaned.filename), 'filepath', eeg_cleaned_visual_swapped_odd.filepath);
+pop_saveset(eeg_cleaned_visual_swapped_stand, 'filename', sprintf('%s_cleaned_visual_swapped_stand.set', eeg_cleaned.filename), 'filepath', eeg_cleaned_visual_swapped_stand.filepath);
+
+cd(dataload_path);
+end
---
+++ Analysis/CleanedDataAnalysis_DifferenceWaves.m
@@ -0,0 +1,163 @@
+%% Chunk 1 - load the data 
+
+% Enter from where you want to load the data of which you want to compute
+% the difference waves
+dataload_path = 'C:\Users\rosah\Desktop\Rosas Unikrams\6th semester from Asus\Parkinson study project\Thesis things\EEG_feelSpace\EEG_data_feelSpace\CleanedDataAnalysis\WithoutBaseline\Averaged'
+cd(dataload_path);
+
+% load all data to plot it
+% note that the trials will be indicated as nr of subjects now (due to eeg_checkset)
+EEG_ankle_swapped_odd = pop_loadset('EEG_cleaned_ankle_swapped_odd_stats.set');
+EEG_ankle_swapped_stand = pop_loadset('EEG_cleaned_ankle_swapped_stand_stats.set');
+EEG_visual_odd = pop_loadset('EEG_cleaned_visual_odd_stats.set');
+EEG_visual_stand = pop_loadset('EEG_cleaned_visual_stand_stats.set');
+EEG_ankle_odd = pop_loadset('EEG_cleaned_ankle_odd_stats.set');
+EEG_ankle_stand = pop_loadset('EEG_cleaned_ankle_stand_stats.set');
+EEG_visual_swapped_odd = pop_loadset('EEG_cleaned_visual_swapped_odd_stats.set');
+EEG_visual_swapped_stand = pop_loadset('EEG_cleaned_visual_swapped_stand_stats.set');
+
+%% Chunk 2 - compute the difference waves
+
+% first we need to compute the difference waves between the standard and
+% the oddball stimulus for each condition
+ankle_swapped_difference = EEG_ankle_swapped_odd.mean - EEG_ankle_swapped_stand.mean;
+ankle_swapped_difference_sem = EEG_ankle_swapped_odd.sem - EEG_ankle_swapped_stand.sem;
+visual_difference = EEG_visual_odd.mean - EEG_visual_stand.mean;
+visual_difference_sem = EEG_visual_odd.sem - EEG_visual_stand.sem;
+ankle_difference = EEG_ankle_odd.mean - EEG_ankle_stand.mean;
+ankle_difference_sem = EEG_ankle_odd.sem - EEG_ankle_stand.sem;
+visual_swapped_difference = EEG_visual_swapped_odd.mean - EEG_visual_swapped_stand.mean;
+visual_swapped_difference_sem = EEG_visual_swapped_odd.sem - EEG_visual_swapped_stand.sem;
+
+
+%% Chunk 3 - Plot ERP difference wave for the Fz and Pz electrode
+
+% our x axis
+time_window = linspace(-200, 600, 205);
+% plot color
+diff_wave_colour = [0.8500 0.3250 0.0980];
+% choose an electrode 
+elec_name = 'Fz';
+elec_nr = 6; %Fz is 6 and Pz is 26
+
+% choose a plot title
+figure_title = sprintf('Electrode %s - ERP difference waves of visual and vibrotactile oddball\n baseline removed', elec_name);
+
+sgtitle(figure_title);
+% create a tiled layout and get the current axis in order to set the legend
+% at the correct position
+tlo = tiledlayout(2,2);
+diff_waves_figure  = gca; 
+
+ax1 = nexttile;
+hold on 
+plot(time_window, ankle_swapped_difference(elec_nr,:,:), 'color', diff_wave_colour) 
+plot(time_window, ankle_swapped_difference(elec_nr,:,:) + ankle_swapped_difference_sem(elec_nr,:,:), ':', 'color', diff_wave_colour)
+plot(time_window, ankle_swapped_difference(elec_nr,:,:) - ankle_swapped_difference_sem(elec_nr,:,:), ':', 'color', diff_wave_colour)
+hold off
+ylim([-2 1]);
+%lgd = legend('odd - standard', 'SEM +', 'SEM -');
+ylabel(sprintf('Amplitude (%sV) in %s',char(181), elec_name));
+title('ankle_swapped condition');
+
+ax2 = nexttile;
+hold on 
+plot(time_window, visual_difference(elec_nr,:,:), 'color', diff_wave_colour) 
+plot(time_window, visual_difference(elec_nr,:,:) + visual_difference_sem(elec_nr,:,:), ':', 'color', diff_wave_colour)
+plot(time_window, visual_difference(elec_nr,:,:) - visual_difference_sem(elec_nr,:,:), ':', 'color', diff_wave_colour)
+hold off
+ylim([-2 1]);
+%lgd = legend('odd - standard', 'SEM +', 'SEM -');
+title('visual condition');
+
+ax3 = nexttile;
+hold on 
+plot(time_window, ankle_difference(elec_nr,:,:), 'color', diff_wave_colour) 
+plot(time_window, ankle_difference(elec_nr,:,:) + ankle_difference_sem(elec_nr,:,:), ':', 'color', diff_wave_colour)
+plot(time_window, ankle_difference(elec_nr,:,:) - ankle_difference_sem(elec_nr,:,:), ':', 'color', diff_wave_colour)
+hold off
+ylim([-2 1]);
+%lgd = legend('odd - standard', 'SEM +', 'SEM -');
+ylabel(sprintf('Amplitude (%sV) in %s',char(181), elec_name));
+xlabel('ms from stimulus onset');
+title('ankle condition');
+
+ax4 = nexttile;
+hold on 
+plot(time_window, visual_swapped_difference(elec_nr,:,:), 'color', diff_wave_colour) 
+plot(time_window, visual_swapped_difference(elec_nr,:,:) + visual_swapped_difference_sem(elec_nr,:,:), ':', 'color', diff_wave_colour)
+plot(time_window, visual_swapped_difference(elec_nr,:,:) - visual_swapped_difference_sem(elec_nr,:,:), ':', 'color', diff_wave_colour)
+hold off
+ylim([-2 1]);
+%lgd = legend('odd - standard', 'SEM +', 'SEM -');
+xlabel('ms from stimulus onset');
+title('visual_swapped condition');
+
+lg  = legend('odd - standard', 'SEM +', 'SEM -','Orientation','Horizontal','NumColumns',3); 
+lg.Layout.Tile = 'South'; % <-- Legend placement with tiled layout
+
+%% Chunk 4 - check the plot and save it
+
+% save the figure as pdf
+saveas(gcf, sprintf('Figures\\%s_difference_waves_WOBaseline', elec_name), 'pdf');
+
+%% Chunk 4 - difference waves of difference waves between body parts
+
+wrist_ankle_diff = ankle_swapped_difference - ankle_difference;
+wrist_ankle_diff_sem = ankle_swapped_difference_sem - ankle_difference_sem;
+wrist_waist_diff = ankle_swapped_difference - waist_difference;
+wrist_waist_diff_sem = ankle_swapped_difference_sem - waist_difference_sem;
+ankle_waist_diff = ankle_difference - waist_difference;
+ankle_waist_diff_sem = ankle_difference_sem - waist_difference_sem;
+
+% plot color
+diff_wave_colour = [0.8500 0.3250 0.0680];
+% choose an electrode 
+elec_name = 'Fz';
+elec_nr = 6; %Fz is 6 and Pz is 26
+
+% choose a plot title
+figure_title = sprintf('Electrode %s - ERP difference waves between body parts\n baseline removed', elec_name);
+sgtitle(figure_title);
+
+% create a tiled layout and get the current axis in order to set the legend
+% at the correct position
+tlo = tiledlayout(2,2);
+diff_waves_figure  = gca; 
+
+ax1 = nexttile;
+hold on 
+plot(time_window, wrist_ankle_diff(elec_nr,:,:))
+plot(time_window, wrist_ankle_diff(elec_nr,: ,:) + wrist_ankle_diff_sem(elec_nr,: ,:), ':')
+plot(time_window, wrist_ankle_diff(elec_nr,: ,:) - wrist_ankle_diff_sem(elec_nr,: ,:), ':')
+hold off
+ylabel(sprintf('Amplitude (%sV) in %s',char(181), elec_name));
+xlabel('ms from stimulus onset');
+title('wrist - ankle');
+
+ax2 = nexttile;
+hold on 
+plot(time_window, wrist_waist_diff(elec_nr,:,:))
+plot(time_window, wrist_waist_diff(elec_nr,:,:) + wrist_waist_diff_sem(elec_nr,:,:), ':')
+plot(time_window, wrist_waist_diff(elec_nr,:,:) - wrist_waist_diff_sem(elec_nr,:,:), ':')
+hold off
+ylabel(sprintf('Amplitude (%sV) in %s',char(181), elec_name));
+xlabel('ms from stimulus onset');
+title('wrist - waist');
+
+ax3 = nexttile;
+hold on 
+plot(time_window, ankle_waist_diff(elec_nr,:,:))
+plot(time_window, ankle_waist_diff(elec_nr,:,:) + ankle_waist_diff_sem(elec_nr,:,:), ':')
+plot(time_window, ankle_waist_diff(elec_nr,:,:) - ankle_waist_diff_sem(elec_nr,:,:), ':')
+hold off
+ylabel(sprintf('Amplitude (%sV) in %s',char(181), elec_name));
+xlabel('ms from stimulus onset');
+title('ankle - waist');
+
+lg  = legend('difference wave', 'SEM +', 'SEM -','Orientation','Horizontal','NumColumns',3); 
+lg.Layout.Tile = 'South'; % <-- Legend placement with tiled layout
+
+%%
+
+saveas(gcf, sprintf('Figures\\%s_diffWaves_bodyparts_WObaseline', elec_name), 'pdf');
